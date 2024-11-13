715cb42ff86e15753f14e905bd10626b818ef4fc
SefaKahramannn <sefakahraman0001@gmail.com>
2024-11-13 21:21:03 +0300

    Rest Assured

--- src/test/java/_01_ApiTest.java
+++ src/test/java/_01_ApiTest.java
@@ -72,6 +72,7 @@ public class _01_ApiTest {
                 // bulunud yeri (path i )vererek içerde assertion hamcrest kütüphaneisi yapıyor
         ;
     }
+
     @Test
     public void checkCountryInResponseBody2(){
         // Soru : "http://api.zippopotam.us/us/90210"  endpoint indne dönen
@@ -113,6 +114,61 @@ public class _01_ApiTest {
                 .then()
                 .log().body()
                 .body("places",hasSize(1)) // places in eleman uzunluğu 1 mi ?
+                .body("places[0].state",equalTo("California"))
+                .body("places.'place name'",hasItem("Beverly Hills"))
         ;
+    }
+
+    @Test
+    public void pathParamTest(){
+        given()
+                .pathParams("ulke","us")
+                .pathParams("postaKodu",90210)
+                .log().uri() // oluşacak endpoint i yazdıralım
+
+                .when()
+                .get("http://api.zippopotam.us/{ulke}/{postaKodu}") //path param
+
+                .then()
+                .log().body()
+        ;
+    }
+
+    @Test
+    public void queryParamTest(){
+        //https://gorest.co.in/public/v1/users?page=1
+
+        given()
+                .param("page",3)
+                .log().uri()
+
+                .when()
+                .get("https://gorest.co.in/public/v1/users") // url ile uri ayrıldı
+
+                .then()
+                .log().body()
+        ;
+    }
+
+    @Test
+    public void queryParamTest2(){
+        // https://gorest.co.in/public/v1/users?page=3
+        // bu linkteki 1 den 10 kadar sayfaları çağırdığınızda response daki donen page degerlerinin
+        // çağrılan page nosu ile aynı olup olmadığını kontrol ediniz.
+
+        for (int i = 1; i <= 10; i++) {
+
+            given()
+                    .param("page",i)
+                    .log().uri()
+
+                    .when()
+                    .get("https://gorest.co.in/public/v1/users") // url ile uri ayrıldı
+
+                    .then()
+                    .log().body()
+                    .body("meta.pagination.page",equalTo(i))
+            ;
+        }
     }
 }
